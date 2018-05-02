67014350a71e8917ade565ec18ed3d1aa1854a48
Olivia Guest <guest.olivia@gmail.com>
2018-05-02 13:58:50 +0100

    I thnik I need to changeCSS framework

--- js/getData.js
+++ js/getData.js
@@ -4,17 +4,24 @@ google.load('visualization', '1', {
 });
 google.setOnLoadCallback(drawChart);
 
-
+function delay(response) {
+  setTimeout(function() {
+    handleQueryResponse(response);
+}, 4000)
+}
 
 function drawChart() {
   // Add your sheets url and range below
   var spreadsheetUrl = "https://docs.google.com/spreadsheets/d/1pDM_cbLpmfbzNFuOJO3JJTm5IF_8KzeqS8wYIsV9RN0/edit";
-  https://docs.google.com/spreadsheets/d/1pDM_cbLpmfbzNFuOJO3JJTm5IF_8KzeqS8wYIsV9RN0/edit#gid=0
-  var query = new google.visualization.Query(spreadsheetUrl);
-  query.send(handleQueryResponse);
+  https: //docs.google.com/spreadsheets/d/1pDM_cbLpmfbzNFuOJO3JJTm5IF_8KzeqS8wYIsV9RN0/edit#gid=0
+    var query = new google.visualization.Query(spreadsheetUrl);
+  query.send(delay);
 }
 
+
+
 function handleQueryResponse(response) {
+
   var dataTable = response.getDataTable();
   var header = '<div class="siimple-table siimple-table--striped siimple-table--hover">';
   // https://developers.google.com/chart/interactive/docs/reference?hl=en#methods
@@ -35,12 +42,10 @@ function handleQueryResponse(response) {
     // while (col < cols) {
     for (c in cols) {
       header += '<div class="siimple-table-cell">'
-      console.log(cols[c], c);
       if (((cols[c] == 2) | (cols[c] == 5) | (cols[c] == 6) | (cols[c] == 7)) & (row > 0)) {
         header += '<a href="' + dataTable.getValue(row, cols[c]) +
-        '">site</a>';
-      }
-      else {
+          '">site</a>';
+      } else {
         // console.log(row, col);
         header += dataTable.getValue(row, cols[c])
       }
