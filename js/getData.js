2aa72777c0f0f22c6ad608a5f777a66f2d64da5f
oliviaguest <guest.olivia@gmail.com>
2018-05-08 10:22:23 +0100

    slow tidy

--- js/getData.js
+++ js/getData.js
@@ -1,139 +1,19 @@
 // https://google,developers.appspot.com/chart/interactive/docs/spreadsheets#gid
-google.load('visualization', '1', {
-  packages: ['corechart', 'line']
-});
-google.setOnLoadCallback(drawChart);
-
-// function delay(response) {
-//   setTimeout(function() {
-//     handleQueryResponse(response);
-//     // test(response);
-// }, 0)
-// }
+google.load('visualization', '1', {packages: ['corechart', 'line']});
 
 function drawChart() {
-  // Add your sheets url and range below
-  var spreadsheetUrl = "https://docs.google.com/spreadsheets/d/1pDM_cbLpmfbzNFuOJO3JJTm5IF_8KzeqS8wYIsV9RN0/edit";
-  https: //docs.google.com/spreadsheets/d/1pDM_cbLpmfbzNFuOJO3JJTm5IF_8KzeqS8wYIsV9RN0/edit#gid=0
+    "use strict";
+
+    // Add your sheets url and range below
+    var spreadsheetUrl = "https://docs.google.com/spreadsheets/d/1pDM_cbLpmfbzNFuOJO3JJTm5IF_8KzeqS8wYIsV9RN0/edit";
     var query = new google.visualization.Query(spreadsheetUrl);
-  query.send(handleQueryResponse);
+    var boop = query.send(handleQueryResponse);
+    console.log(boop);
 }
-var arr = []
+google.setOnLoadCallback(drawChart);
 
+var arr = [];
 
-
-function handleQueryResponse(response) {
-
-  var dataTable = response.getDataTable();
-
-
-  var rows = dataTable.getNumberOfRows();
-  var cols = dataTable.getNumberOfColumns();
-  for (row=0; row < rows; row++){
-    var inner = []
-    for (col=0; col < cols; col++){
-      inner.push(dataTable.getValue(row, col))
-
-    }
-    arr.push(inner)
-  }
-
-  function shuffle(array) {
-    var currentIndex = array.length, temporaryValue, randomIndex;
-
-    // While there remain elements to shuffle...
-    while (0 !== currentIndex) {
-
-      // Pick a remaining element...
-      randomIndex = Math.floor(Math.random() * currentIndex);
-      currentIndex -= 1;
-
-      // And swap it with the current element.
-      temporaryValue = array[currentIndex];
-      array[currentIndex] = array[randomIndex];
-      array[randomIndex] = temporaryValue;
-    }
-
-    return array;
-  }
-
-var shuffledArr= arr.slice(1, -1)
-shuffledArr = shuffle(shuffledArr);
-var newArr = []
-console.log(shuffledArr.length, arr.length)
-
-
-// for (i in shuffledArr){
-//   console.log(i)
-//   for (j in shuffledArr[i]){
-//     arr[i+1][j] = shuffledArr[i][j]
-//   }
-//   // console.log(shuffledArr[i])
-// }
-newArr = []
-newArr[0] = arr[0]
-for (row=0; row < shuffledArr.length; row++){
-  newArr[row+1] = shuffledArr[row]
-     // for (col=0; col < arr[row].length; col++){
-       // arr[row][col] = newArr[row-1][col]
-//
-     // }
-  }
-arr = newArr
-console.log(newArr.length, arr.length)
-
-  var item = document.getElementById("table");
-  var parentDiv = item.parentNode;
-
-  var body, tab, tr, td, tn, row, col;
-  body = document.getElementsByTagName('body')[0];
-  tab = document.createElement('table');
-  // head
-  row = 0
-      thead = document.createElement('thead');
-      tr = document.createElement('tr');
-
-      for (col=0; col < arr[row].length; col++){
-          th = document.createElement('th');
-          th.id = col;
-          th.onclick = function() { sortTable(this.id, col); };
-          arrow = document.createElement("i");
-          arrow.classList.add("fas");
-          arrow.classList.add("fa-angle-up");
-          arrow.classList.add("white-arrow")
-          tn = document.createTextNode(arr[row][col]);
-          th.appendChild(tn);
-          th.appendChild(arrow);
-
-
-
-          tr.appendChild(th);
-
-
-      }
-  thead.appendChild(tr);
-
-  tab.appendChild(thead);
-  // body
-  tbody = document.createElement('tbody');
-  for (row=1; row < arr.length; row++){
-      tr = document.createElement('tr');
-      console.log(row, arr[row])
-      for (col=0; col < arr[row].length; col++){
-          td = document.createElement('td');
-          tn = document.createTextNode(arr[row][col]);
-          td.appendChild(tn);
-          tr.appendChild(td);
-
-      }
-      tbody.appendChild(tr);
-      tab.appendChild(tbody);
-  }
-  tab.id = "table";
-
-  parentDiv.replaceChild(tab, item);
-  // console.log(dataTable.getNumberOfColumns(), dataTable.getNumberOfRows())
-}
 
 // From: https://www.w3schools.com/howto/howto_js_sort_table.asp
 function sortTable(n, n_max) {
@@ -213,4 +93,95 @@ function sortTable(n, n_max) {
       }
     }
   }
+}
+
+function shuffle(array) {
+    "use strict";
+
+    var currentIndex = array.length, temporaryValue, randomIndex;
+
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+
+        // Pick a remaining element...
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+
+        // And swap it with the current element.
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
+    }
+
+    return array;
+}
+
+
+function handleQueryResponse(response) {
+    "use strict";
+    var row, col, inner, shuffledArr, newArr = [], dataTable = response.getDataTable(), rows = dataTable.getNumberOfRows(), cols = dataTable.getNumberOfColumns();
+    for (row = 0; row < rows; row += 1) {
+        inner = [];
+        for (col = 0; col < cols; col += 1) {
+            inner.push(dataTable.getValue(row, col));
+        }
+        arr.push(inner);
+    }
+
+    shuffledArr = arr.slice(1, -1);
+    shuffledArr = shuffle(shuffledArr);
+    console.log(shuffledArr.length, arr.length);
+
+    newArr[0] = arr[0];
+    for (row = 0; row < shuffledArr.length; row += 1) {
+        newArr[row + 1] = shuffledArr[row];
+    }
+    arr = newArr;
+    console.log(newArr.length, arr.length);
+
+    var item = document.getElementById("table"), parentDiv = item.parentNode;
+
+    var body, tab, tr, td, tn, col, thead, th, arrow, tbody;
+    body = document.getElementsByTagName('body')[0];
+    tab = document.createElement('table');
+    // head
+    row = 0
+    thead = document.createElement('thead');
+    tr = document.createElement('tr');
+
+    for (col = 0; col < arr[row].length; col += 1) {
+        th = document.createElement('th');
+        th.id = col;
+        th.onclick = function () { sortTable(this.id, col); };
+        arrow = document.createElement("i");
+        arrow.classList.add("fas");
+        arrow.classList.add("fa-angle-up");
+        arrow.classList.add("white-arrow")
+        tn = document.createTextNode(arr[row][col]);
+        th.appendChild(tn);
+        th.appendChild(arrow);
+        tr.appendChild(th);
+      }
+  thead.appendChild(tr);
+
+  tab.appendChild(thead);
+  // body
+  tbody = document.createElement('tbody');
+  for (row=1; row < arr.length; row++){
+      tr = document.createElement('tr');
+      console.log(row, arr[row])
+      for (col=0; col < arr[row].length; col++){
+          td = document.createElement('td');
+          tn = document.createTextNode(arr[row][col]);
+          td.appendChild(tn);
+          tr.appendChild(td);
+
+      }
+      tbody.appendChild(tr);
+      tab.appendChild(tbody);
+  }
+  tab.id = "table";
+
+  parentDiv.replaceChild(tab, item);
+  // console.log(dataTable.getNumberOfColumns(), dataTable.getNumberOfRows())
 }
