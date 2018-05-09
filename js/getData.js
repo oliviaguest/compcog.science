174a7014dbb21fb9b19d2612148ad45103a9c2c2
Olivia Guest <guest.olivia@gmail.com>
2018-05-09 20:08:37 +0100

    making table more readable

--- js/getData.js
+++ js/getData.js
@@ -1,214 +1,212 @@
+/* global google */
+
 // https://google,developers.appspot.com/chart/interactive/docs/spreadsheets#gid
 google.load('visualization', '1', {
-  packages: ['corechart', 'line']
-});
+    packages: ['corechart', 'line']
+})
 
 function drawChart() {
-  "use strict";
-
-  // Add your sheets url and range below
-  var spreadsheetUrl = "https://docs.google.com/spreadsheets/d/1pDM_cbLpmfbzNFuOJO3JJTm5IF_8KzeqS8wYIsV9RN0/edit";
-  var query = new google.visualization.Query(spreadsheetUrl);
-  var boop = query.send(handleQueryResponse);
-  console.log(boop);
+    'use strict'
+    // Code to get the data from Google Sheets based on: https://stackoverflow.com/a/33055115
+    // Add your sheets url and range below
+    var spreadsheetUrl = 'https://docs.google.com/spreadsheets/d/1pDM_cbLpmfbzNFuOJO3JJTm5IF_8KzeqS8wYIsV9RN0/edit'
+    var query = new google.visualization.Query(spreadsheetUrl)
+    query.send(handleQueryResponse)
 }
-google.setOnLoadCallback(drawChart);
 
+google.setOnLoadCallback(drawChart)
 var arr = [],
-  upSymbol = 'fa-angle-up',
-  downSymbol = 'fa-angle-down'
-var arr = [],
-  upSymbol = 'fa-sort-up',
-  downSymbol = 'fa-sort-down',
-  sortSymbol = 'fa-sort'
+    upSymbol = 'fa-sort-up',
+    downSymbol = 'fa-sort-down',
+    sortSymbol = 'fa-sort'
 
-// From: https://www.w3schools.com/howto/howto_js_sort_table.asp
+// Code to sort table based on: https://www.w3schools.com/howto/howto_js_sort_table.asp
 function sortTable(n, n_max) {
-
-  var table, rows, switching, i, x, y, shouldSwitch, dir, switchcount = 0;
-  table = document.getElementById("table");
-  switching = true;
-  //Set the sorting direction to ascending:
-  dir = "asc";
-
-  rows = table.getElementsByTagName("TR");
-
-  for (i = 0; i < n_max; i++) {
-    rows[0].children[i].getElementsByTagName('i')[0].classList.remove(upSymbol);
-    rows[0].children[i].getElementsByTagName('i')[0].classList.remove(downSymbol);
-
-    rows[0].children[i].getElementsByTagName('i')[0].classList.add(sortSymbol);
-    rows[0].children[i].getElementsByTagName('i')[0].classList.add('disable-sort');
-  }
-  /*Make a loop that will continue until
+    var table, rows, switching, i, x, y, shouldSwitch, dir, switchcount = 0
+    table = document.getElementById('table')
+    switching = true
+    //Set the sorting direction to ascending:
+    dir = 'asc'
+    rows = table.getElementsByTagName('TR')
+    for (i = 0; i < n_max; i++) {
+        rows[0].children[i].getElementsByTagName('i')[0].classList.remove(upSymbol)
+        rows[0].children[i].getElementsByTagName('i')[0].classList.remove(downSymbol)
+        rows[0].children[i].getElementsByTagName('i')[0].classList.add(sortSymbol)
+        rows[0].children[i].getElementsByTagName('i')[0].classList.add('disable-sort')
+    }
+    /*Make a loop that will continue until
   no switching has been done:*/
-  while (switching) {
+    while (switching) {
     //start by saying: no switching is done:
-    switching = false;
-    rows = table.getElementsByTagName("TR");
-    rows[0].children[n].getElementsByTagName('i')[0].classList.remove('disable-sort');
-
-    if (dir == "asc") {
-      rows[0].children[n].getElementsByTagName('i')[0].classList.add(upSymbol);
-      rows[0].children[n].getElementsByTagName('i')[0].classList.remove(downSymbol);
-
-
-    } else {
-      rows[0].children[n].getElementsByTagName('i')[0].classList.add(downSymbol);
-      rows[0].children[n].getElementsByTagName('i')[0].classList.remove(upSymbol);
-
-    }
-    /*Loop through all table rows (except the
+        switching = false
+        rows = table.getElementsByTagName('TR')
+        rows[0].children[n].getElementsByTagName('i')[0].classList.remove('disable-sort')
+        if (dir == 'asc') {
+            rows[0].children[n].getElementsByTagName('i')[0].classList.add(upSymbol)
+            rows[0].children[n].getElementsByTagName('i')[0].classList.remove(downSymbol)
+        } else {
+            rows[0].children[n].getElementsByTagName('i')[0].classList.add(downSymbol)
+            rows[0].children[n].getElementsByTagName('i')[0].classList.remove(upSymbol)
+        }
+        /*Loop through all table rows (except the
     first, which contains table headers):*/
-    for (i = 1; i < (rows.length - 1); i++) {
-      //start by saying there should be no switching:
-      shouldSwitch = false;
-      /*Get the two elements you want to compare,
+        for (i = 1; i < (rows.length - 1); i++) {
+            //start by saying there should be no switching:
+            shouldSwitch = false
+            /*Get the two elements you want to compare,
       one from current row and one from the next:*/
-
-
-      // x = rows[i].getElementsByTagName("TD")[n];
-      x = rows[i].children[n];
-
-      y = rows[i + 1].children[n];
-
-      /*check if the two rows should switch place,
+            // x = rows[i].getElementsByTagName("TD")[n];
+            x = rows[i].children[n]
+            y = rows[i + 1].children[n]
+            /*check if the two rows should switch place,
       based on the direction, asc or desc:*/
-      if (dir == "asc") {
-        if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
-          //if so, mark as a switch and break the loop:
-          shouldSwitch = true;
-          break;
+            if (dir == 'asc') {
+                if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
+                    //if so, mark as a switch and break the loop:
+                    shouldSwitch = true
+                    break
+                }
+            } else if (dir == 'desc') {
+                if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {
+                    //if so, mark as a switch and break the loop:
+                    shouldSwitch = true
+                    break
+                }
+            }
         }
-      } else if (dir == "desc") {
-        if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {
-          //if so, mark as a switch and break the loop:
-          shouldSwitch = true;
-          break;
-        }
-      }
-    }
-    if (shouldSwitch) {
-      /*If a switch has been marked, make the switch
+        if (shouldSwitch) {
+            /*If a switch has been marked, make the switch
       and mark that a switch has been done:*/
-      rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
-      switching = true;
-      //Each time a switch is done, increase this count by 1:
-      switchcount++;
-    } else {
-      /*If no switching has been done AND the direction is "asc",
+            rows[i].parentNode.insertBefore(rows[i + 1], rows[i])
+            switching = true
+            //Each time a switch is done, increase this count by 1:
+            switchcount++
+        } else {
+            /*If no switching has been done AND the direction is "asc",
       set the direction to "desc" and run the while loop again.*/
-      if (switchcount == 0 && dir == "asc") {
-        dir = "desc";
-        switching = true;
-      }
+            if (switchcount == 0 && dir == 'asc') {
+                dir = 'desc'
+                switching = true
+            }
+        }
     }
-  }
 }
 
 function shuffle(array) {
-  "use strict";
-
-  var currentIndex = array.length,
-    temporaryValue, randomIndex;
-
-  // While there remain elements to shuffle...
-  while (0 !== currentIndex) {
-
+    // From: https://stackoverflow.com/a/2450976
+    'use strict'
+    var currentIndex = array.length,
+        temporaryValue, randomIndex
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
     // Pick a remaining element...
-    randomIndex = Math.floor(Math.random() * currentIndex);
-    currentIndex -= 1;
-
-    // And swap it with the current element.
-    temporaryValue = array[currentIndex];
-    array[currentIndex] = array[randomIndex];
-    array[randomIndex] = temporaryValue;
-  }
-
-  return array;
+        randomIndex = Math.floor(Math.random() * currentIndex)
+        currentIndex -= 1
+        // And swap it with the current element.
+        temporaryValue = array[currentIndex]
+        array[currentIndex] = array[randomIndex]
+        array[randomIndex] = temporaryValue
+    }
+    return array
 }
 
-
 function handleQueryResponse(response) {
-  "use strict";
-  var row, col, inner, shuffledArr, newArr = [],
-    dataTable = response.getDataTable(),
-    rows = dataTable.getNumberOfRows(),
-    cols = dataTable.getNumberOfColumns();
-  for (row = 0; row < rows; row += 1) {
-    inner = [];
-    for (col = 0; col < cols; col += 1) {
-      inner.push(dataTable.getValue(row, col));
+    'use strict'
+    var row, col, inner, shuffledArr,
+        dataTable = response.getDataTable(),
+        rows = dataTable.getNumberOfRows(),
+        cols = dataTable.getNumberOfColumns()
+    for (row = 0; row < rows; row += 1) {
+        inner = []
+        for (col = 0; col < cols; col += 1) {
+            inner.push(dataTable.getValue(row, col))
+        }
+        arr.push(inner)
     }
-    arr.push(inner);
-  }
-
-  shuffledArr = arr.slice(1, -1);
-  shuffledArr = shuffle(shuffledArr);
-  console.log(shuffledArr.length, arr.length);
-
-  newArr[0] = arr[0];
-  for (row = 0; row < shuffledArr.length; row += 1) {
-    newArr[row + 1] = shuffledArr[row];
-  }
-  arr = newArr;
-  console.log(newArr.length, arr.length);
-
-  var item = document.getElementById("table"),
-    parentDiv = item.parentNode;
-
-  var body, tab, tr, td, tn, col, thead, th, arrow, tbody, text;
-  body = document.getElementsByTagName('body')[0];
-  tab = document.createElement('table');
-  // head
-  row = 0
-  thead = document.createElement('thead');
-  tr = document.createElement('tr');
-
-  for (col = 0; col < arr[row].length; col += 1) {
-    th = document.createElement('th');
-    th.id = col;
-    th.onclick = function() {
-      sortTable(this.id, col);
-    };
-    arrow = document.createElement("i");
-    arrow.classList.add("fas");
-    arrow.classList.add(sortSymbol);
-    arrow.classList.add('disable-sort')
-    tn = document.createTextNode(arr[row][col]);
-    th.appendChild(tn);
-    th.appendChild(arrow);
-    tr.appendChild(th);
-  }
-  thead.appendChild(tr);
-
-  tab.appendChild(thead);
-  // body
-  tbody = document.createElement('tbody');
-  for (row = 1; row < arr.length; row++) {
-    tr = document.createElement('tr');
-    console.log(row, arr[row])
-    for (col = 0; col < arr[row].length; col++) {
-
-      td = document.createElement('td');
-      td.classList.add(arr[0][col].replace(" ", "-").toLowerCase());
-      text = arr[row][col];
-      if (arr[0][col] == 'Keywords'){
-        if (text != null) {
-        text = text.toLowerCase();
-      }}
-
-      tn = document.createTextNode(text);
-
-      td.appendChild(tn);
-      tr.appendChild(td);
-
+    shuffledArr = shuffle(arr.slice(1, -1))
+    for (row = 0; row < shuffledArr.length; row += 1) {
+        arr[row + 1] = shuffledArr[row]
     }
-    tbody.appendChild(tr);
-    tab.appendChild(tbody);
-  }
-  tab.id = "table";
+    var item = document.getElementById('table'),
+        parentDiv = item.parentNode
+    var tab, tr, td, tn, thead, th, arrow, tbody, text, divThead, divTr, divTh, superDiv
 
-  parentDiv.replaceChild(tab, item);
-  // console.log(dataTable.getNumberOfColumns(), dataTable.getNumberOfRows())
+    // The div head of the table:
+    row = 0
+    divThead = document.createElement('div')
+    divThead.classList.add('thead')
+    divThead.id = 'sticky-head'
+    divTr = document.createElement('div')
+    divTr.classList.add('tr')
+    for (col = 0; col < arr[row].length; col += 1) {
+        divTh = document.createElement('div')
+        divTh.classList.add('th')
+        divTh.id = col
+        divTh.onclick = function() {
+            sortTable(this.id, col)
+        }
+        arrow = document.createElement('i')
+        arrow.classList.add('fas')
+        arrow.classList.add(sortSymbol)
+        arrow.classList.add('disable-sort')
+        tn = document.createTextNode(arr[row][col])
+        divTh.appendChild(tn)
+        divTh.appendChild(arrow)
+        divTr.appendChild(divTh)
+    }
+    divThead.appendChild(divTr)
+
+    superDiv = document.createElement('div')
+    superDiv.appendChild(divThead)
+    tab = document.createElement('table')
+
+    // The head of the table:
+    row = 0
+    thead = document.createElement('thead')
+    tr = document.createElement('tr')
+
+    for (col = 0; col < arr[row].length; col += 1) {
+        th = document.createElement('th')
+        th.id = col
+        th.onclick = function() {
+            sortTable(this.id, col)
+        }
+        arrow = document.createElement('i')
+        arrow.classList.add('fas')
+        arrow.classList.add(sortSymbol)
+        arrow.classList.add('disable-sort')
+        tn = document.createTextNode(arr[row][col])
+        th.appendChild(tn)
+        th.appendChild(arrow)
+        tr.appendChild(th)
+    }
+    thead.appendChild(tr)
+    tab.appendChild(thead)
+    // The body of the table
+    tbody = document.createElement('tbody')
+    for (row = 1; row < arr.length; row++) {
+        tr = document.createElement('tr')
+        for (col = 0; col < arr[row].length; col++) {
+            td = document.createElement('td')
+            td.classList.add(arr[0][col].replace(' ', '-').toLowerCase())
+            text = arr[row][col]
+            if (arr[0][col] == 'Keywords') {
+                if (text != null) {
+                    text = text.toLowerCase()
+                }
+            }
+            tn = document.createTextNode(text)
+            td.appendChild(tn)
+            tr.appendChild(td)
+        }
+        tbody.appendChild(tr)
+        tab.appendChild(tbody)
+    }
+    tab.id = 'table'
+    superDiv.appendChild(tab)
+    parentDiv.replaceChild(superDiv, item)
+
+    $('#sticky-head').sticky({
+        topSpacing: 24
+    })
 }
--- js/jquery.sticky.js
+++ js/jquery.sticky.js
@@ -10,20 +10,20 @@
 //              It will only set the 'top' and 'position' of your element, you
 //              might need to adjust the width in some cases.
 
-(function (factory) {
-    if (typeof define === 'function' && define.amd) {
-        // AMD. Register as an anonymous module.
-        define(['jquery'], factory);
-    } else if (typeof module === 'object' && module.exports) {
-        // Node/CommonJS
-        module.exports = factory(require('jquery'));
-    } else {
-        // Browser globals
-        factory(jQuery);
-    }
-}(function ($) {
-    var slice = Array.prototype.slice; // save ref to original slice()
-    var splice = Array.prototype.splice; // save ref to original slice()
+(function(factory) {
+  if (typeof define === 'function' && define.amd) {
+    // AMD. Register as an anonymous module.
+    define(['jquery'], factory);
+  } else if (typeof module === 'object' && module.exports) {
+    // Node/CommonJS
+    module.exports = factory(require('jquery'));
+  } else {
+    // Browser globals
+    factory(jQuery);
+  }
+}(function($) {
+  var slice = Array.prototype.slice; // save ref to original slice()
+  var splice = Array.prototype.splice; // save ref to original slice()
 
   var defaults = {
       topSpacing: 0,
@@ -67,10 +67,9 @@
             s.stickyElement.trigger('sticky-end', [s]);
             s.currentTop = null;
           }
-        }
-        else {
-          var newTop = documentHeight - s.stickyElement.outerHeight()
-            - s.topSpacing - s.bottomSpacing - scrollTop - extra;
+        } else {
+          var newTop = documentHeight - s.stickyElement.outerHeight() -
+            s.topSpacing - s.bottomSpacing - scrollTop - extra;
           if (newTop < 0) {
             newTop = newTop + s.topSpacing;
           } else {
@@ -79,13 +78,13 @@
           if (s.currentTop !== newTop) {
             var newWidth;
             if (s.getWidthFrom) {
-                padding =  s.stickyElement.innerWidth() - s.stickyElement.width();
-                newWidth = $(s.getWidthFrom).width() - padding || null;
+              padding = s.stickyElement.innerWidth() - s.stickyElement.width();
+              newWidth = $(s.getWidthFrom).width() - padding || null;
             } else if (s.widthFromWrapper) {
-                newWidth = s.stickyWrapper.width();
+              newWidth = s.stickyWrapper.width();
             }
             if (newWidth == null) {
-                newWidth = s.stickyElement.width();
+              newWidth = s.stickyElement.width();
             }
             s.stickyElement
               .css('width', newWidth)
@@ -105,7 +104,7 @@
             if (s.currentTop === s.topSpacing && s.currentTop > newTop || s.currentTop === null && newTop < s.topSpacing) {
               // just reached bottom || just started to stick but bottom is already reached
               s.stickyElement.trigger('sticky-bottom-reached', [s]);
-            } else if(s.currentTop !== null && newTop === s.topSpacing && s.currentTop < newTop) {
+            } else if (s.currentTop !== null && newTop === s.topSpacing && s.currentTop < newTop) {
               // sticky is started && sticked at topSpacing && overflowing from top just finished
               s.stickyElement.trigger('sticky-bottom-unreached', [s]);
             }
@@ -117,7 +116,7 @@
           var stickyWrapperContainer = s.stickyWrapper.parent();
           var unstick = (s.stickyElement.offset().top + s.stickyElement.outerHeight() >= stickyWrapperContainer.offset().top + stickyWrapperContainer.outerHeight()) && (s.stickyElement.offset().top <= s.topSpacing);
 
-          if( unstick ) {
+          if (unstick) {
             s.stickyElement
               .css('position', 'absolute')
               .css('top', '')
@@ -128,7 +127,7 @@
               .css('position', 'fixed')
               .css('top', newTop)
               .css('bottom', '')
-              .css('z-index', '9')//s.zIndex);
+              .css('z-index', '9') //s.zIndex);
           }
         }
       }
@@ -140,14 +139,14 @@
         var s = sticked[i];
         var newWidth = null;
         if (s.getWidthFrom) {
-            if (s.responsiveWidth) {
-                newWidth = $(s.getWidthFrom).width();
-            }
-        } else if(s.widthFromWrapper) {
-            newWidth = s.stickyWrapper.width();
+          if (s.responsiveWidth) {
+            newWidth = $(s.getWidthFrom).width();
+          }
+        } else if (s.widthFromWrapper) {
+          newWidth = s.stickyWrapper.width();
         }
         if (newWidth != null) {
-            s.stickyElement.css('width', newWidth);
+          s.stickyElement.css('width', newWidth);
         }
       }
     },
@@ -165,23 +164,31 @@
 
           stickyElement.wrapAll(function() {
             if ($(this).parent("#" + wrapperId).length == 0) {
-                    return wrapper;
+              return wrapper;
             }
-});
+          });
 
           var stickyWrapper = stickyElement.parent();
 
           if (o.center) {
-            stickyWrapper.css({width:stickyElement.outerWidth(),marginLeft:"auto",marginRight:"auto"});
+            stickyWrapper.css({
+              width: stickyElement.outerWidth(),
+              marginLeft: "auto",
+              marginRight: "auto"
+            });
           }
 
           if (stickyElement.css("float") === "right") {
-            stickyElement.css({"float":"none"}).parent().css({"float":"right"});
+            stickyElement.css({
+              "float": "none"
+            }).parent().css({
+              "float": "right"
+            });
           }
 
           o.stickyElement = stickyElement;
           o.stickyWrapper = stickyWrapper;
-          o.currentTop    = null;
+          o.currentTop = null;
 
           sticked.push(o);
 
@@ -205,7 +212,10 @@
               methods.setWrapperHeight(stickyElement);
             }
           });
-          mutationObserver.observe(stickyElement, {subtree: true, childList: true});
+          mutationObserver.observe(stickyElement, {
+            subtree: true,
+            childList: true
+          });
         } else {
           if (window.addEventListener) {
             stickyElement.addEventListener('DOMNodeInserted', function() {
@@ -234,11 +244,11 @@
           var i = sticked.length;
           while (i-- > 0) {
             if (sticked[i].stickyElement.get(0) === that) {
-                splice.call(sticked,i,1);
-                removeIdx = i;
+              splice.call(sticked, i, 1);
+              removeIdx = i;
             }
           }
-          if(removeIdx !== -1) {
+          if (removeIdx !== -1) {
             unstickyElement.unwrap();
             unstickyElement
               .css({
@@ -247,8 +257,7 @@
                 'top': '',
                 'float': '',
                 'z-index': ''
-              })
-            ;
+              });
           }
         });
       }
@@ -266,8 +275,8 @@
   $.fn.sticky = function(method) {
     if (methods[method]) {
       return methods[method].apply(this, slice.call(arguments, 1));
-    } else if (typeof method === 'object' || !method ) {
-      return methods.init.apply( this, arguments );
+    } else if (typeof method === 'object' || !method) {
+      return methods.init.apply(this, arguments);
     } else {
       $.error('Method ' + method + ' does not exist on jQuery.sticky');
     }
@@ -276,8 +285,8 @@
   $.fn.unstick = function(method) {
     if (methods[method]) {
       return methods[method].apply(this, slice.call(arguments, 1));
-    } else if (typeof method === 'object' || !method ) {
-      return methods.unstick.apply( this, arguments );
+    } else if (typeof method === 'object' || !method) {
+      return methods.unstick.apply(this, arguments);
     } else {
       $.error('Method ' + method + ' does not exist on jQuery.sticky');
     }
