4e448623036be2f0bcf28ffa9cd10b6712fd31c5
Olivia Guest <guest.olivia@gmail.com>
2018-05-11 17:28:40 +0100

    wowie

--- js/getData.js
+++ js/getData.js
@@ -1,4 +1,4 @@
-/* global google */
+/* global google, $, console */
 
 // https://google,developers.appspot.com/chart/interactive/docs/spreadsheets#gid
 google.load('visualization', '1', {
@@ -29,10 +29,16 @@ function sortTable(n, n_max) {
     dir = 'asc'
     rows = table.getElementsByTagName('TR')
     for (i = 0; i < n_max; i++) {
-        rows[0].children[i].getElementsByTagName('i')[0].classList.remove(upSymbol)
-        rows[0].children[i].getElementsByTagName('i')[0].classList.remove(downSymbol)
-        rows[0].children[i].getElementsByTagName('i')[0].classList.add(sortSymbol)
-        rows[0].children[i].getElementsByTagName('i')[0].classList.add('disable-sort')
+        if (isProfile(arr[0][i])) {
+            continue
+        }
+        if (isSortable(arr[0][i])) {
+            document.getElementById(i).getElementsByTagName('i')[0].classList.remove(upSymbol)
+            document.getElementById(i).getElementsByTagName('i')[0].classList.remove(downSymbol)
+            document.getElementById(i).getElementsByTagName('i')[0].classList.add(sortSymbol)
+            document.getElementById(i).getElementsByTagName('i')[0].classList.add('disable-sort')
+        }
+
     }
     /*Make a loop that will continue until
   no switching has been done:*/
@@ -40,13 +46,13 @@ function sortTable(n, n_max) {
     //start by saying: no switching is done:
         switching = false
         rows = table.getElementsByTagName('TR')
-        rows[0].children[n].getElementsByTagName('i')[0].classList.remove('disable-sort')
+        document.getElementById(n).getElementsByTagName('i')[0].classList.remove('disable-sort')
         if (dir == 'asc') {
-            rows[0].children[n].getElementsByTagName('i')[0].classList.add(upSymbol)
-            rows[0].children[n].getElementsByTagName('i')[0].classList.remove(downSymbol)
+            document.getElementById(n).getElementsByTagName('i')[0].classList.add(upSymbol)
+            document.getElementById(n).getElementsByTagName('i')[0].classList.remove(downSymbol)
         } else {
-            rows[0].children[n].getElementsByTagName('i')[0].classList.add(downSymbol)
-            rows[0].children[n].getElementsByTagName('i')[0].classList.remove(upSymbol)
+            document.getElementById(n).getElementsByTagName('i')[0].classList.add(downSymbol)
+            document.getElementById(n).getElementsByTagName('i')[0].classList.remove(upSymbol)
         }
         /*Loop through all table rows (except the
     first, which contains table headers):*/
@@ -110,6 +116,14 @@ function shuffle(array) {
     return array
 }
 
+function isSortable(col) {
+    return col != 'Keywords'
+}
+
+function isProfile(col) {
+    return (col == 'Website') || (col == 'Google Scholar') || (col == 'GitHub') || (col == 'Twitter') || (col == 'ORCiD')
+}
+
 function handleQueryResponse(response) {
     'use strict'
     var row, col, inner, shuffledArr,
@@ -129,7 +143,7 @@ function handleQueryResponse(response) {
     }
     var item = document.getElementById('table'),
         parentDiv = item.parentNode
-    var tab, tr, td, tn, thead, th, arrow, tbody, text, divThead, divTr, divTh, superDiv
+    var tab, tr, td, tn, thead, th, arrow, tbody, text, divThead, divTr, divTh, superDiv, profile, a, div
 
     // The div head of the table:
     row = 0
@@ -139,54 +153,74 @@ function handleQueryResponse(response) {
     divTr = document.createElement('div')
     divTr.classList.add('tr')
     for (col = 0; col < arr[row].length; col += 1) {
+        if (isProfile(arr[row][col])) {
+            continue
+        }
         divTh = document.createElement('div')
         divTh.classList.add('th')
         divTh.id = col
-        divTh.onclick = function() {
-            sortTable(this.id, col)
-        }
-        arrow = document.createElement('i')
-        arrow.classList.add('fas')
-        arrow.classList.add(sortSymbol)
-        arrow.classList.add('disable-sort')
         tn = document.createTextNode(arr[row][col])
         divTh.appendChild(tn)
-        divTh.appendChild(arrow)
+        if (isSortable(arr[row][col])) {
+            divTh.onclick = function() {
+                sortTable(this.id, col)
+            }
+            arrow = document.createElement('i')
+            arrow.classList.add('fas')
+            arrow.classList.add(sortSymbol)
+            arrow.classList.add('disable-sort')
+            divTh.classList.add('sortable')
+            divTh.appendChild(arrow)
+
+        }
         divTr.appendChild(divTh)
     }
+    // Now we add a custom column:
+    divTh = document.createElement('div')
+    divTh.classList.add('th')
+    divTh.id = 'profiles'
+    tn = document.createTextNode('Profiles')
+    divTh.appendChild(tn)
+    divTr.appendChild(divTh)
+
     divThead.appendChild(divTr)
 
     superDiv = document.createElement('div')
     superDiv.appendChild(divThead)
     tab = document.createElement('table')
-
-    // The head of the table:
-    row = 0
-    thead = document.createElement('thead')
-    tr = document.createElement('tr')
-
-    for (col = 0; col < arr[row].length; col += 1) {
-        th = document.createElement('th')
-        th.id = col
-        th.onclick = function() {
-            sortTable(this.id, col)
-        }
-        arrow = document.createElement('i')
-        arrow.classList.add('fas')
-        arrow.classList.add(sortSymbol)
-        arrow.classList.add('disable-sort')
-        tn = document.createTextNode(arr[row][col])
-        th.appendChild(tn)
-        th.appendChild(arrow)
-        tr.appendChild(th)
-    }
-    thead.appendChild(tr)
-    tab.appendChild(thead)
+    //
+    // // The head of the table:
+    // row = 0
+    // thead = document.createElement('thead')
+    // tr = document.createElement('tr')
+    //
+    // for (col = 0; col < arr[row].length; col += 1) {
+    //     th = document.createElement('th')
+    //     th.id = col
+    //     th.onclick = function() {
+    //         sortTable(this.id, col)
+    //     }
+    //     arrow = document.createElement('i')
+    //     arrow.classList.add('fas')
+    //     arrow.classList.add(sortSymbol)
+    //     arrow.classList.add('disable-sort')
+    //     tn = document.createTextNode(arr[row][col])
+    //     th.appendChild(tn)
+    //     th.appendChild(arrow)
+    //     tr.appendChild(th)
+    // }
+    // thead.appendChild(tr)
+    // tab.appendChild(thead)
     // The body of the table
     tbody = document.createElement('tbody')
+    tbody.id = 'table-body'
     for (row = 1; row < arr.length; row++) {
         tr = document.createElement('tr')
         for (col = 0; col < arr[row].length; col++) {
+            if (isProfile(arr[0][col])) {
+                continue
+            }
+            div = document.createElement('div')
             td = document.createElement('td')
             td.classList.add(arr[0][col].replace(' ', '-').toLowerCase())
             text = arr[row][col]
@@ -196,9 +230,53 @@ function handleQueryResponse(response) {
                 }
             }
             tn = document.createTextNode(text)
-            td.appendChild(tn)
+            div.appendChild(tn)
+            td.appendChild(div)
             tr.appendChild(td)
         }
+
+        // Now for the custom column
+        td = document.createElement('td')
+        td.classList.add('profiles')
+        div = document.createElement('div')
+        for (col = 0; col < arr[row].length; col++) {
+            if (isProfile(arr[0][col]) && arr[row][col] != null) {
+                profile = document.createElement('i')
+                a = document.createElement('a')
+                a.title = arr[row][1] + ' ' + arr[row][0] + '\'s ' + arr[0][col]
+                a.href = arr[row][col]
+                if (arr[0][col] == 'Twitter') {
+                    profile.classList.add('fab')
+                    profile.classList.add('fa-twitter')
+                }
+                else if (arr[0][col] == 'GitHub') {
+                    profile.classList.add('fab')
+                    profile.classList.add('fa-github')
+                }
+                else if (arr[0][col] == 'ORCiD') {
+                    profile.classList.add('ai')
+                    profile.classList.add('ai-orcid')
+                }
+                else if (arr[0][col] == 'Google Scholar') {
+                    profile.classList.add('ai')
+                    profile.classList.add('ai-google-scholar')
+                }
+                else if (arr[0][col] == 'Website') {
+                    profile.classList.add('fas')
+                    profile.classList.add('fa-external-link-alt')
+                }
+                a.appendChild(profile)
+                div.appendChild(a)
+                td.appendChild(div)
+            } else {
+                continue
+            }
+
+        }
+
+        tr.appendChild(td)
+
+
         tbody.appendChild(tr)
         tab.appendChild(tbody)
     }
@@ -207,6 +285,58 @@ function handleQueryResponse(response) {
     parentDiv.replaceChild(superDiv, item)
 
     $('#sticky-head').sticky({
-        topSpacing: 24
+        topSpacing: 25
     })
+}
+
+document.addEventListener('scroll', checkSticky)
+
+function isInRange(num, a, b)
+// Find if num is between a and b
+{
+    var min, max
+    if (a >= b) {
+        min = b
+        max = a
+    } else if (b > a) {
+        min = a
+        max = b
+    }
+    return ((num >= min) &&
+    (num <= max))
+}
+
+function outOfView(elem)
+// from: https://stackoverflow.com/a/488073
+{
+    var viewTop = $(window).scrollTop()
+    var viewBottom = viewTop + $(window).height()
+
+    var elemTop = $(elem).offset().top
+    var elemBottom = elemTop + $(elem).height()
+    return ((elemBottom < viewTop) || (elemTop > viewBottom))
+
+}
+
+function timeToStick(elem)
+// from: https://stackoverflow.com/a/488073
+{
+    var viewTop = $(window).scrollTop()
+    var viewBottom = viewTop + $(window).height()
+
+    var elemTop = $(elem).offset().top
+    var elemBottom = elemTop + $(elem).height()
+    return (elemTop <= viewTop)
+
+}
+
+function checkSticky() {
+    console.log(outOfView('#table'))
+    if (outOfView('#table')) {
+        $('#sticky-head').unstick()
+    } else if (timeToStick('#table')) {
+        $('#sticky-head').sticky({
+            topSpacing: 24
+        })
+    }
 }
---
+++ js/headhesive.js-master/CHANGELOG.md
@@ -0,0 +1,33 @@
+# Headhesive changelog
+
+## v1.2.4
+- Fix resize event when using numerical offset
+- Fix destroy method for resize event
+- Changelog corrections
+
+## v1.2.3
+- Readme correction
+
+## v1.2.2
+- Fix UMD support
+
+## v1.2.1
+- Bump version to publish to npm
+
+## v1.2.0
+- Add UMD support
+- Allow to select either the top or bottom of the offset element
+- Update the offset position on browser resize
+- Publish to npm
+- Add license file
+- Make the clone version lower z-index than original. Prevents visual overlap of both when scrolling quickly
+
+## v1.1.1
+- Fix class naming
+
+## v1.1.0
+- Call the init method internally on instance creation
+- Destroy method to only destroy it's instance
+
+## v1.0.0
+- Initial
